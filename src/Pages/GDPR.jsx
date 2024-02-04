8c2e53f2cdb78d3628c660bf392b833fd93acffb
IacobOliver <olialexander08@gmail.com>
2024-02-04 23:23:06 +0200

    point 3

--- src/Pages/GDPR.jsx
+++ src/Pages/GDPR.jsx
@@ -2,35 +2,74 @@ import React from 'react'
 
 export default function GDPR() {
 
-    const Introduction = () =>{
+    const Introduction = () => {
         return <div>intro</div>
     }
 
-     const Point1 = () =>{
+    const Point1 = () => {
         return <div>point1</div>
     }
 
-    const Point2 = () =>{
+    const Point2 = () => {
         return <div>point1</div>
     }
 
-    const Point3 = () =>{
+    const Point3 = () => {
+        return <div className='my-4'>
+            <p id="title" className='font-bold underline my-1 text-base'>3. Temeiul Prelucrării Datelor dumneavoastră cu caracter personal</p>
+            <p className='my-1'>In general, prelucrarea datelor cu caracter personal este necesara in vederea desfasurarii activitatii specifice scopurilor prelucrarii.</p>
+            <p className='my-1'>Vi se solicita sa furnizati datele dumneavoastra cu caracter personal, doar in limita strict necesara  in mod obligatoriu, cu exceptia situatiilor limitate in care va vom cere si acordul pentru  anumite informatii facultative. Daca nu ne oferiti respectivele date minimale cerute a fi furnizate, procesul poate fi intarziat sau chiar imposibil.</p>
+            <p className='my-1'>Pentru a putea prelucra datele dumneavoastra cu caracter personal, cabinetul  se bazeaza pe urmatoarele <strong>temeiuri juridice:</strong></p>
+           
+            <div id="list">
+                <p className=' my-1 '>•        <strong>Consimțământul</strong> - <span className='underline'>Preambul (32), (42), (43); Art. 6 alin (1) lit. (a) </span> Prelucrarea este permisă dacă persoana vizată și-a dat consimțământul pentru prelucrare;</p>
+                <p className=' my-1 '>•        <strong>Încheierea sau executarea unui contract</strong> - <span className='underline'>Preambul (44); Art. 6 alin. (1) lit. (b)</span> prelucrarea este necesară pentru executarea unui contract la care persoana vizată este parte sau pentru a face demersuri la cererea persoanei vizate înainte de încheierea unui contract;</p>
+                <p className=' my-1 '>•        <strong>Îndeplinirea unei obligații legale </strong>- <span className='underline'>Preambul (45); Art.6(1) lit. (c);Art. 6 alin. (3)</span> prelucrarea este necesară în vederea îndeplinirii unei obligații legale care îi revine operatorului;</p>
+                <p className=' my-1 '>•        <strong>Interesele vitale</strong> - <span className='underline'>Preambul (46); Art.6(1) lit. (d) </span> prelucrarea este necesara pentru a proteja interesele vitale ale persoanei vitale ale persoane vizate sau ale altei persoane fizice,</p>
+                <p className=' my-1 '>•        <strong>Interesul public </strong>- <span className='underline'>Preambul (47), (48); Art.6(1) lit. (f)</span> prelucrarea este necesară în scopul intereselor legitime urmărite de operator sau de o parte terță, cu excepția cazului în care prevalează interesele sau drepturile și libertățile fundamentale ale persoanei vizate, care necesită protejarea datelor cu caracter personal, în special atunci când persoana vizată este un copil</p>
+                <p className=' my-1 '>•        <strong>Prelucrarea de categorii speciale de date cu caracter personal</strong> - <span className='underline'>Preambul (51)-(56); Art.9  </span></p>
+                <div className='ml-9'>
+                    <p className=' my-2 '>➢        persoana vizată și-a dat consimțământul explicit; </p>
+                    <p className=' my-2 '>➢        prelucrarea este necesară în scopul îndeplinirii obligațiilor și al exercitării unor drepturi specifice ale operatorului sau ale persoanei vizate în domeniul ocupării forței de muncă și al securității sociale și protecției sociale; </p>
+                    <p className=' my-2 '>➢        prelucrarea este necesară pentru protejarea intereselor vitale ale persoanei vizate sau ale unei alte persoane fizice, atunci când persoana vizată se află în incapacitate fizică sau juridică de a-și da consimțământul; </p>
+                    <p className=' my-2 '>➢        prelucrarea este efectuată în cadrul activităților lor legitime și cu garanții adecvate de către o fundație, o asociație sau orice alt organism fără scop lucrativ și cu specific politic, filozofic, religios sau sindical, cu condiția ca prelucrarea să se refere numai la membrii sau la foștii membri ai organismului respectiv sau la persoane cu care acesta are contacte permanente în legătură cu scopurile sale și ca datele cu caracter personal să nu fie comunicate terților fără consimțământul persoanelor vizate; </p>
+                    <p className=' my-2 '>➢        prelucrarea se referă la date cu caracter personal care sunt făcute publice în mod manifest de către persoana vizată; </p>
+                    <p className=' my-2 '>➢        prelucrarea este necesară pentru constatarea, exercitarea sau apărarea unui drept în instanță sau ori de câte ori instanțele acționează în exercițiul funcției lor judiciare; </p>
+                    <p className=' my-2 '>➢        prelucrarea este necesară din motive de interes public major, în baza dreptului Uniunii sau a dreptului intern, care este proporțional cu obiectivul urmărit, respectă esența dreptului la protecția datelor și prevede măsuri corespunzătoare și specifice pentru protejarea drepturilor fundamentale și a intereselor persoanei vizate; </p>
+                    <p className=' my-2 '>➢        prelucrarea este necesară în scopuri legate de medicina preventivă sau a muncii, de evaluarea capacității de muncă a angajatului, de stabilirea unui diagnostic medical, de furnizarea de asistență medicală sau socială sau a unui tratament medical sau de gestionarea sistemelor și serviciilor de sănătate sau de asistență socială, în temeiul dreptului Uniunii sau al dreptului intern sau în temeiul unui contract încheiat cu un cadru medical și sub rezerva respectării condițiilor și garanțiilor prevăzute la alineatul (3); </p>
+                    <p className=' my-2 '>➢        prelucrarea este necesară din motive de interes public în domeniul sănătății publice, cum ar fi protecția împotriva amenințărilor transfrontaliere grave la adresa sănătății sau asigurarea de standarde ridicate de calitate și siguranță a asistenței medicale și a medicamentelor sau a dispozitivelor medicale, în temeiul dreptului Uniunii sau al dreptului intern, care prevede măsuri adecvate și specifice pentru protejarea drepturilor și libertăților persoanei vizate, în special a secretului profesional; </p>
+                    <p className=' my-2 '>➢        prelucrarea este necesară în scopuri de arhivare în interes public, în scopuri de cercetare științifică sau istorică ori în scopuri statistice, în conformitate cu articolul 89 alineatul (1), în baza dreptului Uniunii sau a dreptului intern, care este proporțional cu obiectivul urmărit, respectă esența dreptului la protecția datelor și prevede măsuri corespunzătoare și specifice pentru protejarea drepturilor fundamentale și a intereselor persoanei vizate. </p>
+                </div>
+
+                <p className=' my-1 '>•        <strong>Prelucrarea în alt scop </strong> - <span className='underline'>Art. 6 alin. (4)  </span> În cazul în care prelucrarea în alt scop decât cel pentru care datele cu caracter personal au fost colectate nu se bazează pe consimțământul persoanei vizate sau pe dreptul Uniunii sau dreptul intern, care constituie o măsură necesară și proporțională într-o societate democratică pentru a proteja obiectivele menționate la articolul 23 alineatul (1), operatorul, pentru a stabili dacă prelucrarea în alt scop este compatibilă cu scopul pentru care datele cu caracter personal au fost colectate inițial, ia în considerare, printre altele:</p>
+                <div className='ml-9'>
+                    <p className=' my-2 '>➢        orice legătură dintre scopurile în care datele cu caracter personal au fost colectate și scopurile prelucrării ulterioare preconizate; </p>
+                    <p className=' my-2 '>➢        contextul în care datele cu caracter personal au fost colectate, în special în ceea ce privește relația dintre persoanele vizate și operator; </p>
+                    <p className=' my-2 '>➢        natura datelor cu caracter personal, în special în cazul prelucrării unor categorii speciale de date cu caracter personal, în conformitate cu articolul 9, sau în cazul în care sunt prelucrate date cu caracter personal referitoare la condamnări penale și infracțiuni, în conformitate cu articolul 10; </p>
+                    <p className=' my-2 '>➢        posibilele consecințe asupra persoanelor vizate ale prelucrării ulterioare preconizate; </p>
+                    <p className=' my-2 '>➢        (e)existența unor garanții adecvate, care pot include criptarea sau pseudonimizarea. </p>
+                </div>
+            </div>
+
+            <p className='mt-9 mb-2'>GARANŢII, RESPONSABILITĂŢI.</p>
+            <p>Informaţiile de pe acest website sunt oferite ca atare şi fără nici un fel de garanţie, expresă sau implicită, inclusiv (dar fără limitare la) orice garanţii mercantile implicite, potrivire pentru orice scop, sau neîncalcarea drepturilor unor terţe părţi. În timp ce informaţiile oferite sunt considerate corecte, pot include erori. <strong> Cabinet de Avocat-Individual de Insolventa Bradu Nicoleta-Dorina </strong> nu va fi responsabilă în niciun fel faţă de nicio persoană pentru daune speciale sau indirecte în legatură cu acest material, decât dacă acestea sunt cauzate de neglijenţa sau inducere în eroare în mod intenţionat. Cabinet de Avocat-Individual de Insolventa Bradu Nicoleta-Dorina nu este răspunzatoare pentru conţinutul website-urilor ce sunt administrate de terţe părţi şi, în consecinţa, neagă orice răspundere pentru orice link care face legătură între acest website şi un altul.</p>
+
+        </div>
+    }
+
+    const Point4 = () => {
         return <div>point1</div>
     }
 
-    const Point4 = () =>{
+    const Point5 = () => {
         return <div>point1</div>
     }
 
-    const Point5 = () =>{
+    const Point6 = () => {
         return <div>point1</div>
     }
 
-    const Point6 = () =>{
-        return <div>point1</div>
-    }
-
-    const Point7 = () =>{
+    const Point7 = () => {
         return <div>point1</div>
     }
 
@@ -52,6 +91,7 @@ export default function GDPR() {
                     </div>
 
                     <div id="description" className="text-sm ">
+                        • ➢
                         {/* <p className="my-2.5">Bine ați venit la Cabinet de Avocat-Cabinet Individual de Insolventa Bradu Nicoleta-Dorina!</p>
                         <p className="my-2.5">Punem accent pe integritate, competență și relații de încredere cu clienții noștri. Fie că aveți nevoie de asistență în dreptul afacerilor, dreptul familiei sau oricare altă sferă juridică, suntem aici să vă ghidăm și să vă reprezentăm cu profesionalism și devotament.</p>
                         <p className="my-2.5 font-bold">Cabinetul nostru acoperă toate nevoile de consultanță ale mediului de afaceri: avocatură, mediere, fiscalitate și politici financiare, gestiunea afacerii, insolvență, recuperări creanțe și altele. </p>
@@ -60,13 +100,13 @@ export default function GDPR() {
                         <p className="my-2.5">Pe plan finaciar, colaboram cu avocati, experti contabili /auditori membrii CECCAR si CAFR care asigura optimizarea activitatii, gestionarea dosarelor, colaborarea optima in vederea identificarii celor mai bune solutii, in vederea reorganizarii sau lichidarii, dupa caz, a societatilor aflate in procedura instituita de Legea 85/2014 privind procedurile de prevenire a insolventei si de insolventa. </p>
                         <p className="my-9 "><i>„A fi avocat nu este doar o vocație. Este o încredere publică și fiecare dintre noi are obligația de a da înapoi comunităților noastre.” – <span className="font-bold">Janet Reno</span></i></p> */}
                         <Introduction />
-                        <Point1/>
-                        <Point2/>
-                        <Point3/>
-                        <Point4/>
-                        <Point5/>
-                        <Point6/>
-                        <Point7/>
+                        <Point1 />
+                        <Point2 />
+                        <Point3 />
+                        <Point4 />
+                        <Point5 />
+                        <Point6 />
+                        <Point7 />
                     </div>
 
                 </div>
